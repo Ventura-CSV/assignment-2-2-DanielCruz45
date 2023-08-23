6e0a3e66978cfa4ece0029986b417fa0a55c8bc1
DanielCruz45 <dcruz44@ucmerced.edu>
2023-08-23 08:52:02 -0700

    code set up, lacks math and format

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def main():
     Use the same variables: celcius fahrenheit 
     ##################################################
     """
+    #input value, askes user for some degrees cekcius. added text to clarify what to input.
+    celcius = int(input('Degrees Celcius: '))
 
+    #translates celcius to fahrenheit using the related equation
+    fahrenheit= 0
+
+    #outputs the calcualted fahrenheit in the desired format
+    print(fahrenheit)
     """
     ########################################
     # Do not delete the return statement
